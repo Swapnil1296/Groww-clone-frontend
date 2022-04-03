f1ad256ed2faf0eddb4234b5f76759dd685b1f41
jyotiranjan98 <96143764+jyotiranjan98@users.noreply.github.com>
2022-04-03 20:46:18 +0530

    little change

--- groww_clone Prj/scripts/signup.js
+++ groww_clone Prj/scripts/signup.js
@@ -1,58 +1,60 @@
+var cur = JSON.parse(localStorage.getItem("link"));
+  console.log(cur);
+  document.getElementById("ln").value = cur;
+// function setFormMessage(formElement, type, message) {
 
-function setFormMessage(formElement, type, message) {
+//     var messageElement = formElement.querySelector(".form__message");
 
-    var messageElement = formElement.querySelector(".form__message");
-
-    messageElement.textContent = message;
-    messageElement.classList.add(`form__message--${type}`)
-}
-
-
-
-function signUp(e) {
-    e.preventDefault();
-    let signup_form = document.getElementById("signup");
-
-    let firstName = signup_form.firstName.value;
-    let lastName = signup_form.lastName.value;
-    let email = signup_form.email.value;
-    let password = signup_form.password.value;
-    let mobileNumber = signup_form.mobileNumber.value;
-
-    if (localStorage.getItem('users') === null) {
-        localStorage.setItem('users', JSON.stringify([]))
-    }
+//     messageElement.textContent = message;
+//     messageElement.classList.add(`form__message--${type}`)
+// }
 
 
 
-    let user = {
-        firstName,
-        lastName,
-        email,
-        password,
-        mobileNumber
-    }
+// function signUp(e) {
+//     e.preventDefault();
+//     let signup_form = document.getElementById("signup");
 
-    if (user.mobileNumber.length === 10) {
-        let arr = JSON.parse(localStorage.getItem('users'))
-        let exist = arr.length && JSON.parse(localStorage.getItem("users")).some(user => user.email === email || user.mobileNumber === mobileNumber);
+//     let firstName = signup_form.firstName.value;
+//     let lastName = signup_form.lastName.value;
+//     let email = signup_form.email.value;
+//     let password = signup_form.password.value;
+//     let mobileNumber = signup_form.mobileNumber.value;
 
-        if (!exist) {
-            arr.push(user)
-            localStorage.setItem('users', JSON.stringify(arr));
-            window.location.href = "/login.html"
+//     if (localStorage.getItem('users') === null) {
+//         localStorage.setItem('users', JSON.stringify([]))
+//     }
 
-        } else {
-            signup_form.addEventListener("submit", e => {
-                setFormMessage(signup_form, 'error', "User alerady exist...! Please Login")
-                signup_form.classList.remove("form__message--error")
-            })
-        }
 
-    } else {
-        signup_form.addEventListener("submit", e => {
-            setFormMessage(signup_form, 'error', "Please enter 10 Digit Mobile Number")
-            signup_form.classList.remove("form__message--error")
-        })
-    }
-}
+
+//     let user = {
+//         firstName,
+//         lastName,
+//         email,
+//         password,
+//         mobileNumber
+//     }
+
+//     if (user.mobileNumber.length === 10) {
+//         let arr = JSON.parse(localStorage.getItem('users'))
+//         let exist = arr.length && JSON.parse(localStorage.getItem("users")).some(user => user.email === email || user.mobileNumber === mobileNumber);
+
+//         if (!exist) {
+//             arr.push(user)
+//             localStorage.setItem('users', JSON.stringify(arr));
+//             window.location.href = "/login.html"
+
+//         } else {
+//             signup_form.addEventListener("submit", e => {
+//                 setFormMessage(signup_form, 'error', "User alerady exist...! Please Login")
+//                 signup_form.classList.remove("form__message--error")
+//             })
+//         }
+
+//     } else {
+//         signup_form.addEventListener("submit", e => {
+//             setFormMessage(signup_form, 'error', "Please enter 10 Digit Mobile Number")
+//             signup_form.classList.remove("form__message--error")
+//         })
+//     }
+// }
